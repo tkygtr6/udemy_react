fa3594726233770bc35c5fe841b5ca160cbc6392
tkygtr6 <tkygtr6@gmail.com>
2018-03-16 14:54:30 +0900

    GeocodeResultコンポーネントの実装

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-        };
+        this.state = {};
     }
 
-    handlePlaceSubmit(place){
+    handlePlaceSubmit(place) {
         console.log(place);
     }
 
@@ -16,10 +16,16 @@ class App extends Component {
         return (
             <div>
                 <h1>緯度経度検索</h1>
-                <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+                <SearchForm onSubmit={place => this.handlePlaceSubmit(place)}/>
+                <GeocodeResult
+                    address={this.state.address}
+                    lat={this.state.lat}
+                    lng={this.state.lng}
+                />
             </div>
         );
     }
 }
+
 
 export default App;
